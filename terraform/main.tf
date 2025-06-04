d483a04e005dab0b09c02ef39d22cecf2a5d4f53
Avni <roiavni@gmail.com>
2025-06-04 18:20:16 +0300

    Update Terraform config and Kubernetes YAMLs

--- terraform/main.tf
+++ terraform/main.tf
@@ -72,11 +72,11 @@ resource "aws_eks_cluster" "eks" {
 }
 
 resource "aws_iam_role" "eks_node_role" {
-  name = "eksNodeRole"
+  name = "eksNodeRole-RoiAvni"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
-    Statement = [ {
+    Statement = [{
       Effect = "Allow",
       Principal = {
         Service = "ec2.amazonaws.com"
